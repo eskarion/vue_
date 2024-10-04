32694d27ee1291ba67c9503383fa1ddd619a93dd
Flo <flo@dasdickechamaele.on>
2024-10-04 13:00:09 +0200

    added startpage and mdi icons

--- src/components/startPage.vue
+++ src/components/startPage.vue
@@ -1,7 +1,54 @@
-<script></script>
+<script>
+import { useProjectStore } from '@/stores/projectStore'
+import { storeToRefs } from 'pinia'
+import { mdiFolderEditOutline, mdiOpenInNew, mdiDeleteOutline } from '@mdi/js'
+
+export default {
+  name: 'navBar',
+  setup() {
+    const projectStore = useProjectStore()
+    const { projects } = storeToRefs(projectStore)
+    projectStore.fetchProjects()
+
+    return {
+      projects,
+      fetchProjects: projectStore.fetchProjects,
+      mdiFolderEditOutline,
+      mdiOpenInNew,
+      mdiDeleteOutline
+    }
+  },
+  created() {
+    console.log('created')
+  }
+}
+</script>
 
 <template>
-  <h1 class="text-center">Projektübersicht</h1>
+  <h1 class="text-center mt-5 mb-5 text-h4">Projektübersicht</h1>
+
+  <v-row>
+    <v-col cols="12" md="4" v-for="project in projects" :key="project.id">
+      <v-card>
+        <template v-slot:title> {{ project.name }} </template>
+
+        <template v-slot:text>
+          {{ project.description }}
+        </template>
+        <v-card-actions class="d-flex justify-end">
+          <v-btn-group density="compact" variant="outlined" divided>
+            <v-btn size="small"> <v-icon :icon="mdiFolderEditOutline" color="primary" /></v-btn>
+            <v-btn color="teal-darken-4" :to="`/project/${project.id}`">
+              <v-icon :icon="mdiOpenInNew" color="teal-darken-4" />
+            </v-btn>
+            <v-btn base-color="red-darken-4">
+              <v-icon :icon="mdiDeleteOutline" color="red-darken-4"
+            /></v-btn>
+          </v-btn-group>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
 </template>
 
 <style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import './assets/main.css'
 import router from './router'
+import App from './App.vue'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
@@ -10,13 +11,24 @@ import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
-import App from './App.vue'
+import { aliases, mdi } from 'vuetify/iconsets/mdi-svg'
+import '@mdi/font/css/materialdesignicons.css'
+
 const vuetify = createVuetify({
   theme: {
     defaultTheme: 'dark'
   },
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi
+    }
+  },
   components,
   directives
 })
+
 const pinia = createPinia()
+
 createApp(App).use(router).use(vuetify).use(pinia).mount('#app')
