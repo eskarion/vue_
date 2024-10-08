047e2821621dc5ee6f505ee28ae10969fc90e91f
Flo <flo@dasdickechamaele.on>
2024-10-08 23:27:08 +0200

    changed to options apii, added edit modal

--- src/components/startPage.vue
+++ src/components/startPage.vue
@@ -1,41 +1,56 @@
 <script>
-import { ref } from 'vue'
 import { useProjectStore } from '@/stores/projectStore'
-import { storeToRefs } from 'pinia'
 import { mdiFolderEditOutline, mdiOpenInNew, mdiDeleteOutline } from '@mdi/js'
+import { mapState, mapActions } from 'pinia'
 
 export default {
-  setup() {
-    const projectStore = useProjectStore()
-    const { projects } = storeToRefs(projectStore)
-
-    const deleteDialog = ref(false)
-    const projectToDelete = ref(null)
-
-    projectStore.fetchProjects()
-
-    const openDeleteDialog = (id) => {
-      projectToDelete.value = id
-      deleteDialog.value = true
-    }
-
-    const confirmDelete = async () => {
-      console.log(`confirmDelete is triggered`)
-      await projectStore.deleteProject(projectToDelete.value)
-      deleteDialog.value = false
-    }
-
+  data() {
     return {
-      projects,
-      fetchProjects: projectStore.fetchProjects,
+      deleteDialog: false,
+      projectToDelete: null,
+      editDialog: false,
+      projectToEdit: null,
+      newProjectName: '',
+      newProjectDescription: '',
       mdiFolderEditOutline,
       mdiOpenInNew,
-      mdiDeleteOutline,
-      deleteDialog,
-      projectToDelete,
-      openDeleteDialog,
-      confirmDelete
+      mdiDeleteOutline
     }
+  },
+  computed: {
+    ...mapState(useProjectStore, ['projects'])
+  },
+  methods: {
+    ...mapActions(useProjectStore, ['fetchProjects', 'deleteProject', 'editProject']),
+    openDeleteDialog(id) {
+      this.projectToDelete = id
+      this.deleteDialog = true
+    },
+    async confirmDelete() {
+      await this.deleteProject(this.projectToDelete)
+      this.deleteDialog = false
+    },
+    openEditDialog(project) {
+      this.projectToEdit = project.id
+      this.projectToEditName = project.name
+      this.projectToEditDescription = project.description
+      this.newProjectName = project.name
+      this.newProjectDescription = project.description
+      this.editDialog = true
+    },
+    async confirmEdit() {
+      console.log('ConfirmEdit triggered')
+      const updatedProject = {
+        id: this.projectToEdit,
+        name: this.newProjectName,
+        description: this.newProjectDescription
+      }
+      await this.editProject(updatedProject)
+      this.editDialog = false
+    }
+  },
+  mounted() {
+    this.fetchProjects()
   }
 }
 </script>
@@ -45,7 +60,7 @@ export default {
 
   <v-row>
     <v-col cols="12" md="4" v-for="project in projects" :key="project.id">
-      <v-card style="min-height: 160px">
+      <v-card style="min-height: 160px" :elevation="10">
         <template v-slot:title> {{ project.name }} </template>
 
         <template v-slot:text>
@@ -53,7 +68,9 @@ export default {
         </template>
         <v-card-actions class="d-flex justify-end">
           <v-btn-group density="compact" variant="outlined" divided>
-            <v-btn size="small"> <v-icon :icon="mdiFolderEditOutline" color="primary" /></v-btn>
+            <v-btn @click="openEditDialog(project)">
+              <v-icon :icon="mdiFolderEditOutline" color="primary"
+            /></v-btn>
             <v-btn color="teal-darken-4" :to="`/project/${project.id}`">
               <v-icon :icon="mdiOpenInNew" color="teal-darken-4" />
             </v-btn>
@@ -66,15 +83,44 @@ export default {
     </v-col>
   </v-row>
 
-  <!-- Bestätigungs-Modal -->
+  <!-- Delete-Modal -->
   <v-dialog v-model="deleteDialog" max-width="500px">
-    <!-- Hier verwenden wir deleteDialog -->
     <v-card>
       <v-card-title class="headline">Bist du sicher?</v-card-title>
       <v-card-text> Dieses Projekt wird dauerhaft gelöscht. </v-card-text>
       <v-card-actions>
         <v-btn color="green" text @click="deleteDialog = false">Abbrechen</v-btn>
         <v-btn color="red" text @click="confirmDelete">Löschen</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+
+  <!-- Edit-Modal-->
+  <v-dialog v-model="editDialog" max-width="500px">
+    <v-card>
+      <v-card-title class="headline">Ändere "{{ projectToEditName }}"</v-card-title>
+      <v-divider></v-divider>
+      <v-card-text
+        ><v-form>
+          <div class="text-subtitle-1 text-medium-emphasis">Neuer Projektname:</div>
+          <v-text-field
+            v-model="newProjectName"
+            label="neuer Name"
+            :placeholder="projectToEditName"
+          ></v-text-field>
+          <div class="text-subtitle-1 text-medium-emphasis">Neue ProjektBeschreibung:</div>
+          <v-textarea
+            v-model="newProjectDescription"
+            label="Neue Beschreibung"
+            :placeholder="projectToEditDescription"
+            maxlength="120"
+            counter
+            single-line
+          ></v-textarea> </v-form
+      ></v-card-text>
+      <v-card-actions>
+        <v-btn color="red-darken-4" text @click="editDialog = false">Abbrechen</v-btn>
+        <v-btn color="green" text @click="confirmEdit">Speichern</v-btn>
       </v-card-actions>
     </v-card>
   </v-dialog>
